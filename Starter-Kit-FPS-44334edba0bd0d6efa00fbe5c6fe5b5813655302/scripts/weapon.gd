218e588c7107e361e478b38ee5c63753d1b72a2b
maidangkhanh <mai.d.khanh@gmail.com>
2023-11-27 00:04:55 -0800

    Add weapons: sniper and submachine

--- Starter-Kit-FPS-44334edba0bd0d6efa00fbe5c6fe5b5813655302/scripts/weapon.gd
+++ Starter-Kit-FPS-44334edba0bd0d6efa00fbe5c6fe5b5813655302/scripts/weapon.gd
@@ -4,8 +4,8 @@ class_name Weapon
 @export_subgroup("Model")
 @export var model:PackedScene # Model of the weapon
 @export var position:Vector3 # On-screen position
-@export var rotation:Vector3 # On-screen rotation
-@export var muzzle_position:Vector3 # On-screen position of muzzle flash
+@export var rotation:Vector3 = Vector3(0, 180, 0)# On-screen rotation
+@export var muzzle_position:Vector3 = Vector3(0.1, -0.4, 1.5) # On-screen position of muzzle flash
 
 @export_subgroup("Properties")
 @export_range(0.1, 1) var cooldown: float = 0.1 # Firerate
@@ -13,7 +13,7 @@ class_name Weapon
 @export_range(0, 100) var damage: float = 25 # Damage per hit
 @export_range(0, 5) var spread: float = 0 # Spread of each shot
 @export_range(1, 5) var shot_count: int = 1 # Amount of shots
-@export_range(0, 50) var knockback: int = 20 # Amount of knockback
+#@export_range(0, 50) var knockback: int = 20 # Amount of knockback
 @export_range(1, 5) var reload_time:float = 2 # Time to Reload
 @export_range(1, 200) var max_ammo:int = 30 # Maximum ammunitions
 @export_range(0, 1) var recoil_rate:float = 0.5 # Recoil rate, higher mean more recoil
