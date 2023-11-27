a1e7bb92c661882e242e8493b4a79e8db37feca9
maidangkhanh <mai.d.khanh@gmail.com>
2023-11-26 23:55:28 -0800

    update scoreboard

--- Starter-Kit-FPS-44334edba0bd0d6efa00fbe5c6fe5b5813655302/scenes/scoreboard.gd
+++ Starter-Kit-FPS-44334edba0bd0d6efa00fbe5c6fe5b5813655302/scenes/scoreboard.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+@onready var player_score = preload("res://objects/PlayerScore.tscn")
+@onready var score_talble = $PanelContainer/MarginContainer/ScoreTable
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
@@ -17,3 +18,11 @@ func _input(event):
 		
 	if Input.is_action_just_released("scoreboard"):
 		hide()
+
+
+func add_player(name:String):
+	var score_instance = player_score.instantiate()
+	var name_label:Label = score_instance.get_node("Container/LabelName")
+	name_label.set_text(name)
+	
+	score_talble.add_child(score_instance)
